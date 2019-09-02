cb77590d4d7aa84033a11f464b3a50843214ba62
Cyberghost <cghost@cghost2.1.1>
2019-09-02 13:18:19 -0400

    log added

--- vulnserver-python.py
+++ vulnserver-python.py
@@ -3,8 +3,10 @@ from threading import Thread
 
 _SERVER = '0.0.0.0'
 _PORT = 8989
+_LOG_FILE = ""
 
 def conn(client, addr):
+    global _LOG_FILE
     try:
         client.send("CYBERGHOST V2.1 :  Python Command Processor \r\n>>")
 
@@ -17,20 +19,29 @@ def conn(client, addr):
                 command = "python -c \'" +  data.strip() + "\'"
         
                 response = subprocess.check_output(command, shell=True).strip()
-                print(str(addr[0]) + " " + str(addr[1]) + " " + command + "\t" + " ".join(response.split()))
+                log = str(addr[0]) + " " + str(addr[1]) + " " + command + "\t" + " ".join(response.split()) + "\n"
+
+		print(log)
+		_LOG_FILE.write(log)
+
                 client.send(">>" + str(response) + "\n>>")
             except subprocess.CalledProcessError as ex:
+		#print(ex)
                 client.close()
                 break
             except IOError as ex:
+		#print(ex)
                 if ex.errno == errno.EPIPE or ex.errno == errno.ECONNRESET:
                     break
                 pass
-    except :
+    except Exception as ex:
+	#print(ex)
         client.close()
 
 def main():
     print("[+] Starting CYBERGHOST V2.1 Server")
+    global _LOG_FILE
+    _LOG_FILE = open('/etc/vulnserver/vuln.log', 'a+')
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
